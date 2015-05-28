1862493dfb7be534c0abd9d2754a3d5b1e24142a
Richard Ma <richard.ma.19850509@gmail.com>
2015-05-29 06:33:10 +0800

    add pac files for ss

--- sync_examples.sh
+++ sync_examples.sh
@@ -2,4 +2,6 @@
 
 sed -e "/^var IP_ADDRESS/cvar IP_ADDRESS = '127.0.0.1:7070';" -e "/^var PROXY_TYPE/cvar PROXY_TYPE = 'SOCKS5';" whitelist.pac > examples/whitelist_socks5_7070.pac
 sed -e "/^var IP_ADDRESS/cvar IP_ADDRESS = '127.0.0.1:8081';" -e "/^var PROXY_TYPE/cvar PROXY_TYPE = 'PROXY';" whitelist.pac > examples/whitelist_http_8081.pac
+# For ShadowSocks [SS]
+sed -e "/^var IP_ADDRESS/cvar IP_ADDRESS = '127.0.0.1:1080';" -e "/^var PROXY_TYPE/cvar PROXY_TYPE = 'SOCKS5';" whitelist.pac > examples/whitelist_for_ss_socks5_1080.pac
 
